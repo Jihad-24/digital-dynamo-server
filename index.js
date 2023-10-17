b5b3368d37eb15dd4b07ec8324c64b7ab83bdff9
Jihad-24 <mdjihat0021@example.com>
2023-10-17 22:51:43 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,139 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require("cors");
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5001;
+
+// middleware 
+app.use(express.json());
+app.use(cors());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8urwnno.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const productCollection = client.db('productDB').collection('product');
+        const userCollection = client.db('productDB').collection('user');
+
+        // coffiee related apis
+
+        app.get('/product', async (req, res) => {
+            const cursor = productCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const quary = { _id: new ObjectId(id) };
+            const result = await productCollection.findOne(quary);
+            res.send(result);
+        })
+
+        app.post('/product', async (req, res) => {
+            const newCoffiee = req.body;
+            console.log(newCoffiee);
+            const result = await productCollection.insertOne(newCoffiee)
+            res.send(result)
+        })
+
+        app.put('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updatedCoffiee = req.body;
+            const coffiee = {
+                $set: {
+                    name: updatedCoffiee.name,
+                    quantity: updatedCoffiee.quantity,
+                    supplier: updatedCoffiee.supplier,
+                    taste: updatedCoffiee.taste,
+                    category: updatedCoffiee.category,
+                    details: updatedCoffiee.details,
+                    photo: updatedCoffiee.photo
+                }
+            }
+            const result = await productCollection.updateOne(filter, coffiee, options)
+            res.send(result);
+
+        })
+
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const quary = { _id: new ObjectId(id) };
+            const result = await productCollection.deleteOne(quary);
+            res.send(result);
+        })
+
+        // user related apis
+
+        app.get('/user', async (req, res) => {
+            const cursor = userCollection.find();
+            const users = await cursor.toArray();
+            res.send(users);
+        })
+
+        app.post('/user', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+        // app.patch('/user', async (req, res) => {
+        //     const user = req.body;
+        //     const filter = { email: user.email }
+        //     const updateUser = {
+        //         $set: {
+        //             email: user.email,
+        //             lastSignInTime: user.lastSignInTime
+        //         }
+        //     }
+        //     const result = await userCollection.updateOne(filter, updateUser)
+        //     res.send(result);
+        // })
+
+        // app.delete('/user/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const quary = { _id: new ObjectId(id) };
+        //     const result = await userCollection.deleteOne(quary);
+        //     res.send(result);
+        // })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get("/", (req, res) => {
+    res.send("my DigitalDynamo server is running...");
+});
+
+
+app.listen(port, () => {
+    console.log(`Simple Crud is Running on port ${port}`);
+});
+
